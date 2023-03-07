0fe2d86391315a9a490a992db676b7dceede867c
kyynk <zhan.kiliinn@gmail.com>
2023-03-07 16:53:59 +0800

    merge main(vin)

--- Source/Game/mygame_initialize.cpp
+++ Source/Game/mygame_initialize.cpp
@@ -5,7 +5,6 @@
 #include "../Library/audio.h"
 #include "../Library/gameutil.h"
 #include "../Library/gamecore.h"
-#include "config.h"
 #include "mygame.h"
 
 using namespace game_framework;
@@ -19,11 +18,10 @@ CGameStateInit::CGameStateInit(CGame *g) : CGameState(g)
 
 void CGameStateInit::OnInit()
 {
-	logo.LoadBitmapByString({ "img/ao oni_action/oni_00.bmp" }, RGB(204, 255, 0));
+	//
 	// 當圖很多時，OnInit載入所有的圖要花很多時間。為避免玩遊戲的人
 	//     等的不耐煩，遊戲會出現「Loading ...」，顯示Loading的進度。
 	//
-	TRACE("ok\n");
 	ShowInitProgress(0, "Start Initialize...");	// 一開始的loading進度為0%
 	//
 	// 開始載入資料
@@ -40,17 +38,14 @@ void CGameStateInit::OnBeginState()
 
 void CGameStateInit::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
 {
-	if (nChar == VK_SPACE) {
-		GotoGameState(GAME_STATE_RUN);
-	}
+
 }
 
 void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
 {
-
+	GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
 }
+
 void CGameStateInit::OnShow()
-{	
-	logo.SetTopLeft(SIZE_X/2  - logo.GetWidth()/2,SIZE_Y/2 - logo.GetHeight());
-	logo.ShowBitmap();
+{
 }
