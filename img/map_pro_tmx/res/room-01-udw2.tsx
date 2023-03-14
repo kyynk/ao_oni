d1424ed89964099150a8191902eac9d1ae1d0ced
kyynk <zhan.kiliinn@gmail.com>
2023-03-15 00:35:15 +0800

    add map.project and add tileset in map.project

---
+++ img/map_pro_tmx/res/room-01-udw2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="room-01-udw2" tilewidth="32" tileheight="32" tilecount="1056" columns="8">
+ <image source="../../Graphics/Tilesets/room-01-udw2.png" width="256" height="4224"/>
+</tileset>
---
+++ img/map_pro_tmx/res/room-01-udz.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="room-01-udz" tilewidth="32" tileheight="32" tilecount="896" columns="8">
+ <image source="../../Graphics/Tilesets/room-01-udz.png" width="256" height="3584"/>
+</tileset>
